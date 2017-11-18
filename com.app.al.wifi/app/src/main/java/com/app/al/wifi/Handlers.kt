a929965344347bcfb2bb710fcbca191a466b5865
alflaylah <yugiki@gmail.com>
2017-11-18 11:37:44 +0900

    kotlinのバージョンアップ

--- com.app.al.wifi/app/src/main/java/com/app/al/wifi/Handlers.kt
+++ com.app.al.wifi/app/src/main/java/com/app/al/wifi/Handlers.kt
@@ -1,23 +1,21 @@
 package com.app.al.wifi
 
 import android.content.ContentValues
-import android.databinding.BindingAdapter
 import android.databinding.BindingMethod
 import android.databinding.BindingMethods
 import android.support.v4.widget.SwipeRefreshLayout
 import android.util.Log
-import com.app.al.wifi.viewmodel.WifiListViewModel
 
 /*
  * Copyright DMM.com Lab Co.,Ltd.
  */
 @BindingMethods(
-  BindingMethod(type = SwipeRefreshLayout::class, attribute = "onRefresh", method = "setOnRefreshListener")
+    BindingMethod(type = SwipeRefreshLayout::class, attribute = "onRefresh", method = "setOnRefreshListener")
 )
 object Handlers {
 
-    @JvmStatic
-    fun onRefresh() {
-      Log.d(ContentValues.TAG, "onRefresh")
+  @JvmStatic
+  fun onRefresh() {
+    Log.d(ContentValues.TAG, "onRefresh")
   }
 }
--- com.app.al.wifi/app/src/main/java/com/app/al/wifi/viewmodel/WifiListViewModel.kt
+++ com.app.al.wifi/app/src/main/java/com/app/al/wifi/viewmodel/WifiListViewModel.kt
@@ -1,10 +1,8 @@
 package com.app.al.wifi.viewmodel
 
 import android.content.ContentValues.TAG
-import android.databinding.BindingAdapter
 import android.databinding.ObservableBoolean
 import android.net.wifi.ScanResult
-import android.support.v4.widget.SwipeRefreshLayout
 import android.util.Log
 import java.io.Serializable
 
